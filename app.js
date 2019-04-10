ba3575224d1531d5914cae5b99b509fc2b088f53
Samarth Keskar <samarthkeskar363@gmail.com>
2019-04-10 12:02:44 +0530

    admin routes completed

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongo = require("mongodb");
 
 const app = express();
 
+const Admin = require("./routes/Admin/admin")
+
 app.set("view engine", "hbs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +17,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
+
+app.use("/admin",Admin);
 
 
 
---
+++ routes/Admin/admin.js
@@ -0,0 +1,24 @@
+"use strict";
+const express = require('express');
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+    res.render('adminLogin.hbs');
+});
+
+router.post('/login', (req, res) => {
+    res.redirect('/admin/home');
+});
+
+
+router.get('/home', (req, res) => {
+    res.render("adminPanel.hbs");
+});
+
+
+router.get('/home/video', (req, res) => {
+    res.render("adminApp.hbs");
+});
+
+
+module.exports = router;
